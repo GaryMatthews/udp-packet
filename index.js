07973a03b5a680d0309491c94c7c40b47795a240
James Halliday <mail@substack.net>
2015-10-16 18:31:08 -0700

    separate checksum

--- index.js
+++ index.js
@@ -1,35 +1,53 @@
 exports.encode = function (packet) {
   var data = packet.data, len = data.length
-  var srcport = packet.sourcePort, srcip = packet.sourceIp
-  var dstport = packet.destinationPort, dstip = packet.destinationIp
+  var srcport = packet.sourcePort, dstport = packet.destinationPort
   var buf = new Buffer(len + 8)
   buf.writeUInt16BE(srcport, 0)
   buf.writeUInt16BE(dstport, 2)
   buf.writeUInt16BE(buf.length, 4)
   var protocol = packet.protocol === undefined ? 0x11 : packet.protocol
-  var checksum = 0xffff
-  // pseudo header: srcip (16), dstip (16), 0 (8), proto (8), udp len (16)
-  if (srcip && dstip) {
-    if (typeof srcip === 'string') srcip = Buffer(srcip.split('.'))
-    if (typeof dstip === 'string') dstip = Buffer(dstip.split('.'))
-    checksum = 0
-    var pad = len % 2
-    for (var i = 0; i < len + pad; i += 2) {
-      checksum += ((data[i]<<8)&0xff00) + ((data[i+1])&0xff)
-    }
-    for (var i = 0; i < 4; i += 2) {
-      checksum += ((dstip[i]<<8)&0xff00) + (dstip[i+1]&0xff)
-    }
-    checksum += protocol + len
-    while (checksum>>16) {
-      checksum = (checksum & 0xffff) + (checksum >> 16)
-    }
-    checksum = 0xffff ^ checksum
-  }
-  buf.writeUInt16BE(checksum, 6)
+  buf.writeUInt16BE(checksum(packet), 6)
   packet.data.copy(buf, 8)
   return buf
 }
 
 exports.decode = function (buf) {
+  var len = buf.readUInt16BE(4)
+  return {
+    sourcePort: buf.readUInt16BE(0),
+    destinationPort: buf.readUInt16BE(2),
+    length: len,
+    checksum: buf.readUInt16BE(6),
+    data: buf.slice(8, len)
+  }
+}
+
+exports.checksum = checksum
+function checksum (packet) {
+  // pseudo header: srcip (16), dstip (16), 0 (8), proto (8), udp len (16)
+  var data = packet.data, len = data.length
+  var srcport = packet.sourcePort, srcip = packet.sourceIp
+  var dstport = packet.destinationPort, dstip = packet.destinationIp
+  if (!srcip || !dstip) return 0xffff
+  var protocol = packet.protocol === undefined ? 0x11 : packet.protocol
+  var sum = 0xffff
+  // pseudo header: srcip (16), dstip (16), 0 (8), proto (8), udp len (16)
+  if (srcip && dstip) {
+    if (typeof srcip === 'string') srcip = Buffer(srcip.split('.'))
+    if (typeof dstip === 'string') dstip = Buffer(dstip.split('.'))
+    sum = 0
+    var pad = len % 2
+    for (var i = 0; i < len + pad; i += 2) {
+      sum += ((data[i]<<8)&0xff00) + ((data[i+1])&0xff)
+    }
+    for (var i = 0; i < 4; i += 2) {
+      sum += ((dstip[i]<<8)&0xff00) + (dstip[i+1]&0xff)
+    }
+    sum += protocol + len
+    while (sum>>16) {
+      sum = (sum & 0xffff) + (sum >> 16)
+    }
+    sum = 0xffff ^ sum
+  }
+  return sum
 }
